e863869061e6562195a21a578f463d8ce7d34a54
Melissa Real <melissa.real@hotmail.com>
2018-01-11 16:32:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,3 +29,8 @@ function accessElementInArray(array, index){
 function destructivelyRemoveElementFromBeginningOfArray(array){
  return array.shift();
 }
+
+function removeElementFromBeginningOfArray(array){
+  var newArry = array.slice[1];
+  return newArry;
+}
