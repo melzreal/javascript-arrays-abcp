90f7d7f826ebcc76c95ac6f7d4865c0f54e49480
Melissa Real <melissa.real@hotmail.com>
2018-01-11 16:22:47 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, x){
+  var newArr = [x, ...array];
+  return newArr;
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, y){
+   array.push(y);
+  return array;
+}
+
+function addElementToEndOfArray(array, x){
+  var newArr = [ ...array, x];
+  return newArr;
+}
+
+
+function destructivelyAddElementToEndOfArray(array, y){
+  array.unshift(y);
+  return array;
+}
