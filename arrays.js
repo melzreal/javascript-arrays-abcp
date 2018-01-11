4d60405f4f0012532383b37780097fbbf509f0f1
Melissa Real <melissa.real@hotmail.com>
2018-01-11 16:39:31 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,47 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, x){
+  var newArr = [x, ...array];
+  return newArr;
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, y){
+  array.unshift(y);
+  return array;
+}
+
+function addElementToEndOfArray(array, x){
+  var newArr = [ ...array, x];
+  return newArr;
+}
+
+
+function destructivelyAddElementToEndOfArray(array, y){
+  array.push(y);
+  return array;
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  var newArry = array.slice(1);
+  return newArry;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  var nArr = array.slice(0, array.length-1);
+  return nArr;
+}
